62921ee4954f6c59df00190ee66c7d86549f8521
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-22 13:23:28 +0100

    Add AppScreen with labels and destinations for main app views

--- Journal/Navigation/AppScreen.swift
+++ Journal/Navigation/AppScreen.swift
@@ -7,12 +7,47 @@
 
 import SwiftUI
 
-struct AppScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+enum AppScreen: Codable, Hashable, Identifiable, CaseIterable {
+    case trips
+    case steps
+    case placemarks
+    case stats
+    case settings
+
+    var id: AppScreen { self }
+}
+
+extension AppScreen {
+    @ViewBuilder
+    var label: some View {
+        switch self {
+        case .trips:
+            Label("Trips", systemImage: "airplane")
+        case .steps:
+            Label("Steps", systemImage: "figure.walk")
+        case .placemarks:
+            Label("Locations", systemImage: "mappin.and.ellipse")
+        case .stats:
+            Label("Statistics", systemImage: "chart.bar.xaxis")
+        case .settings:
+            Label("Steps", systemImage: "gear")
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .trips:
+            TripView()
+        case .steps:
+            StepView()
+        case .placemarks:
+            PlacemarkView()
+        case .stats:
+            StatisticsView()
+        case .settings:
+            SettingsView()
+        }
     }
 }
 
-#Preview {
-    AppScreen()
-}
